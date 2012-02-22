87172d0e247f381cfb8868e1b4389fae63633c6f
BATTISTA YANN <batyan@squeeze.iut.univ-aix.fr>
2012-02-22 14:55:48 +0100

    Completed boat class

--- src/model/Boat.java
+++ src/model/Boat.java
@@ -11,13 +11,62 @@ public class Boat implements Serializable {
 	private String boatName;
 	private int serialNum;
 	private String insurance;
+	
+	@Override
+	public String toString() {
+		return "Boat [boatName=" + boatName + ", serialNum=" + serialNum
+				+ ", insurance=" + insurance + "]";
+	}
 
-	/*
-	 * public Boat(int numEt, String nomEt, String prenomEt, String cpEt,
-	 * String villeEt, int annee, int groupe) { this.numEt = numEt; this.nomEt =
-	 * nomEt; this.prenomEt = prenomEt; this.cpEt = cpEt; this.villeEt =
-	 * villeEt; this.annee = annee; this.groupe = groupe; }
-	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((boatName == null) ? 0 : boatName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Boat other = (Boat) obj;
+		if (boatName == null) {
+			if (other.boatName != null)
+				return false;
+		} else if (!boatName.equals(other.boatName))
+			return false;
+		return true;
+	}
+
+	public String getBoatName() {
+		return boatName;
+	}
+
+	public void setBoatName(String boatName) {
+		this.boatName = boatName;
+	}
+
+	public int getSerialNum() {
+		return serialNum;
+	}
+
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+	public String getInsurance() {
+		return insurance;
+	}
+
+	public void setInsurance(String insurance) {
+		this.insurance = insurance;
+	}
 
 	public Boat() {
 
