3a88d61709549b78b54ca7b679a198d5dda56064
BATTISTA YANN <batyan@squeeze.iut.univ-aix.fr>
2012-02-22 14:48:12 +0100

    Boat class

--- src/model/Boat.java
+++ src/model/Boat.java
@@ -1,5 +1,27 @@
 package model;
 
-public class Boat {
+import java.io.Serializable;
 
+public class Boat implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6405580716160297097L;
+
+	private String boatName;
+	private int serialNum;
+	private String insurance;
+
+	/*
+	 * public Boat(int numEt, String nomEt, String prenomEt, String cpEt,
+	 * String villeEt, int annee, int groupe) { this.numEt = numEt; this.nomEt =
+	 * nomEt; this.prenomEt = prenomEt; this.cpEt = cpEt; this.villeEt =
+	 * villeEt; this.annee = annee; this.groupe = groupe; }
+	 */
+
+	public Boat() {
+
+	}
+
+	
 }
