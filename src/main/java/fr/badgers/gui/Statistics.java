866666382bb2f262dd7eae545107e7a50a6e30cc
Eric Gillet <fear_hardcore@yahoo.fr>
2012-04-01 02:56:51 +0200

    Statistic GUI done, need Listeners (use this tab as an reference for future design

--- src/main/java/fr/badgers/gui/Statistics.java
+++ src/main/java/fr/badgers/gui/Statistics.java
@@ -1,8 +1,18 @@
 package fr.badgers.gui;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
 import javax.persistence.EntityManager;
-import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
+import javax.swing.SwingConstants;
 
 public class Statistics extends JPanel{
 	EntityManager em;
@@ -14,7 +24,50 @@ public class Statistics extends JPanel{
 	public Statistics(EntityManager em) {
 		super();
 		this.em = em;
-		this.add(new JButton());
+		
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		
+		JPanel propPanel = new JPanel();
+		propPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+		propPanel.add(new JLabel("Propriétaire"));
+		propPanel.add(new JComboBox());
+		JTextField propDisplay = new JTextField("N/A",4);
+		propPanel.add(propDisplay);
+		
+		JSeparator separ = new JSeparator(SwingConstants.HORIZONTAL);
+		separ.setPreferredSize(new Dimension(10,10));
+		this.add(separ);
+		
+		JPanel boatPanel = new JPanel();
+		boatPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+		boatPanel.add(new JLabel("Bateau"));
+		boatPanel.add(new JComboBox());
+		JTextField boatDisplay = new JTextField("N/A",4);
+		boatPanel.add(boatDisplay);
+		
+		JPanel modelPanel = new JPanel();
+		modelPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+		modelPanel.add(new JLabel("Modèle"));
+		modelPanel.add(new JComboBox());
+		JTextField modelDisplay = new JTextField("N/A",4);
+		modelPanel.add(modelDisplay);
+		
+		JPanel bassinPanel = new JPanel();
+		bassinPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+		bassinPanel.add(new JLabel("Bassin"));
+		bassinPanel.add(new JComboBox());
+		bassinPanel.add(new JLabel("Nombre de sorties depuis"));
+		bassinPanel.add(new JSpinner(new SpinnerDateModel()));
+		JTextField bassinDisplay = new JTextField("N/A",4);
+		bassinPanel.add(bassinDisplay);
+		
+		this.add(propPanel);
+		this.add(new JSeparator(JSeparator.HORIZONTAL));
+		this.add(boatPanel);
+		this.add(new JSeparator(JSeparator.HORIZONTAL));
+		this.add(modelPanel);
+		this.add(new JSeparator(JSeparator.HORIZONTAL));
+		this.add(bassinPanel);
 	}
 
 }
